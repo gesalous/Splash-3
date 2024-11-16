2cbb928004b16cafdc6f8cc7fb9da3e2f0cf36aa
Giorgos Saloustros <gesalous@ics.forth.gr>
2024-11-16 15:13:16 +0200

    [feat] Add barrier and m4 macros implementation

--- ges_locks/include/ges_barrier.h
+++ ges_locks/include/ges_barrier.h
@@ -4,12 +4,11 @@
 typedef struct {
     volatile uint64_t counter;     // Tracks number of arrived threads
     volatile uint64_t flag;       // Sense flag for barrier synchronization
-    volatile int num_threads;        // Total number of threads to wait for
 } ges_barrier_t;
 
-void ges_barrier_init(ges_barrier_t *barrier, int num_threads);
+void ges_barrier_init(ges_barrier_t *barrier);
 
-void ges_barrier_wait(ges_barrier_t *barrier);
+void ges_barrier_wait(ges_barrier_t *barrier, uint64_t num_proc);
 
 // Destroy the barrier
 void ges_barrier_destroy(ges_barrier_t *barrier);
--- ges_locks/src/ges_barrier.c
+++ ges_locks/src/ges_barrier.c
@@ -1,19 +1,18 @@
 #include "../include/ges_barrier.h"
 #include <stdlib.h>
-
-void ges_barrier_init(ges_barrier_t *barrier, int num_threads) {
-  barrier->counter = num_threads;
+#include <stdio.h>
+void ges_barrier_init(ges_barrier_t *barrier) {
+  barrier->counter = 0;
   barrier->flag = 0;
-  barrier->num_threads = num_threads;
 }
 
-void ges_barrier_wait(ges_barrier_t *barrier) {
+void ges_barrier_wait(ges_barrier_t *barrier, uint64_t num_proc) {
     uint64_t current_flag = barrier->flag;
-    
+    fprintf(stderr, "Barrier target is %lu\n",num_proc); 
     // Decrement counter and get previous value
-    if (1 == __sync_fetch_and_sub(&barrier->counter, 1)) {
-        barrier->counter = barrier->num_threads; 
-        barrier->flag = !current_flag; 
+    if (num_proc != __sync_fetch_and_add(&barrier->counter, 1)) {
+        barrier->counter = 0;
+        barrier->flag = !current_flag;
     } else {
         // Wait for flag to change
         while (barrier->flag == current_flag) {
